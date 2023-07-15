66355c044cd080caa730ea5c7151f3c32b3ce215
Goutham K B <goutham79007@gmail.com>
2023-07-15 13:34:06 +0530

    calculator done

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  result =
+    parseInt(principle.value) +
+    (parseInt(rate.value) * parseInt(years.value)) / 100;
+  console.log(result);
 }
-        
+
+rate.addEventListener("input", () => {
+  rateValue.innerText = rate.value + "%";
+});
---
+++ vendor.js
@@ -0,0 +1,5 @@
+let principle = document.querySelector("#principal");
+let rate = document.querySelector("#rate");
+let years = document.querySelector("#years");
+let rateValue = document.querySelector(".rateValue");
+let result = 0;
