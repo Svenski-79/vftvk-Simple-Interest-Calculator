f0b965ec82cffa6822e43274281cdc4ef488b12e
Goutham K B <goutham79007@gmail.com>
2023-07-15 16:29:01 +0530

    added output promp,some clean up

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+const date = new Date();
 function compute() {
   result =
     parseInt(principle.value) +
-    (parseInt(rate.value) * parseInt(years.value)) / 100;
-  console.log(result);
+    (parseInt(rate.value) * parseInt(years.value) * parseInt(principle.value)) /
+      100;
+  returnYear = date.getFullYear() + parseInt(years.value);
+  console.log(returnYear);
+  resultText.innerText = `if you deposit ${principle.value} at an interest rate of ${rate.value}%, you will receive an amount of ${result} in ${returnYear} years`;
 }
 
 rate.addEventListener("input", () => {
   rateValue.innerText = rate.value + "%";
+});
+rateValue.addEventListener("input", () => {
+  rate.value = parseInt(rateValue.innerText);
 });
--- vendor.js
+++ vendor.js
@@ -2,4 +2,5 @@ let principle = document.querySelector("#principal");
 let rate = document.querySelector("#rate");
 let years = document.querySelector("#years");
 let rateValue = document.querySelector(".rateValue");
+let resultText = document.querySelector("#result");
 let result = 0;
